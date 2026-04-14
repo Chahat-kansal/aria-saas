7dbfd12e3373cc35f965740ad2d8b950f594a004
Chahat-kansal <39122702+Chahat-kansal@users.noreply.github.com>
2026-04-14 13:17:34 +1000

    Update Sidebar.tsx

--- src/components/chat/Sidebar.tsx
+++ src/components/chat/Sidebar.tsx
@@ -92,7 +92,7 @@ export function Sidebar({ user }: Props) {
         className={`fixed md:relative z-40 flex flex-col w-[280px] md:w-64 bg-[#16161d] border-r border-white/5 transition-transform duration-200 ${
           open ? 'translate-x-0' : '-translate-x-full md:translate-x-0'
         }`}
-        style={{ height: '100dvh' }}
+        style={{ height: '100%', flexShrink: 0 }}
       >
 
         {/* Logo */}
